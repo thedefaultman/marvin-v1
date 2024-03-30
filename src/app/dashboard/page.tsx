8e8c8da2ec3ab001d695765c3f6258b6dd78a8b3
Daniel Zoghalchali <danielha.abraham@gmail.com>
2024-03-30 14:32:41 -0700

    2:40 dashboard and prisma update. File model and upload button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { redirect } from 'next/navigation'
+import { db } from '@/db'
+import Dashboard from '@/components/Dashboard'
+
+
+
 const page = async () => {
 
     //get current user from kinde
@@ -8,7 +13,16 @@ const page = async () => {
     const user = await getUser()
   
     if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
-    return <div>{user.email}</div>
+
+    const dbUser = await db.user.findFirst({
+        where : {
+            id: user.id
+        }
+    })
+
+    if (!dbUser) redirect('/auth-callback?origin=dashboard')
+
+    return <Dashboard />
 }
 
 export default page
